e7f455a74e198fa96f4b63c48334b2948080fadf
Boris <soskinboris@gmail.com>
2025-10-24 17:49:01 +0300

    home work #3 new method≠

--- home-work-3/Order.py
+++ home-work-3/Order.py
@@ -6,7 +6,6 @@ class Order:
 
     def __init__(self, products: dict[Product, int]) -> None:
         self.products = products
-        print(products)
 
     # -add_product(product, quantity) — метод для добавления товара в заказ. Если товара недостаточно на складе, должно выдаваться сообщение об ошибке;
     def add_product(self, product: Product, quantity: int) -> None:
@@ -26,3 +25,19 @@ class Order:
         for product, quantity in self.products.items():
             result += product.price * quantity
         return result
+
+    # . Реализуйте возможность удаления товаров из заказа.
+    # -Добавьте метод remove_product(product, quantity) в класс Order, который будет удалять указанное количество товара из заказа.
+    # Если количество товара в заказе становится равным нулю, удалите товар из словаря products.
+    def remove_product(self, product: Product, quantity: int) -> None:
+        ordered_qty = self.products[product]
+        if quantity > ordered_qty:
+            raise ValueError(f"Cannot remove {quantity}; only {ordered_qty} in order")
+
+        new_qty = ordered_qty - quantity
+        if new_qty == 0:
+            del self.products[product]
+        else:
+            self.products[product] = new_qty
+
+        product.update_stock(quantity)
